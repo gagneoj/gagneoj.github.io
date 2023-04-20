426a84ca7c3f68477b785aeba1d77a944bd7b905
gagneoj <128319547+gagneoj@users.noreply.github.com>
2023-04-20 09:56:21 -0400

    Final Java Script Upload
    This is the final upload for my javascript files

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let mybutton = document.getElementById("btn");
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.documentElement.scrollTop > 30) {
       mybutton.style.display = "block";
     } else {
       mybutton.style.display = "none";
     }
   }
 function returnFunction() {
-    document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
